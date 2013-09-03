0559ba697586b687c12d343d0be164803a73d6f1
cikelengfeng <502805412@qq.com>
2013-09-03 22:27:34 +0800

    添加注释

--- src/com/yuedu/utils/JSONObjectAndStringCollectionCoder.java
+++ src/com/yuedu/utils/JSONObjectAndStringCollectionCoder.java
@@ -7,11 +7,13 @@ import org.json.JSONObject;
 import java.util.Collection;
 
 /**
+ * 用于将一个泛型集合转换成另一种泛型集合，并且两个集合的元素是一一对应的，对应关系由调用者提供的Converter实例决定
+ * e.g. cast ArrayList<JSONObject> to ArrayList<String>
  * Created by dong on 13-9-3.
  */
 public class JSONObjectAndStringCollectionCoder {
 
-    public static final Collection<String> JSONObjectCollectionToString(@NotNull Collection<JSONObject> jsonObjects) {
+    public static Collection<String> JSONObjectCollectionToString(@NotNull Collection<JSONObject> jsonObjects) {
         return convertCollection(jsonObjects,new Converter<String, JSONObject>() {
             @Override
             public String convert(JSONObject from) {
@@ -20,7 +22,7 @@ public class JSONObjectAndStringCollectionCoder {
         });
     }
 
-    public static final Collection<JSONObject> StringionToJSONObjectCollect(@NotNull Collection<String> jsonStrings) {
+    public static Collection<JSONObject> StringionToJSONObjectCollect(@NotNull Collection<String> jsonStrings) {
         return convertCollection(jsonStrings,new Converter<JSONObject, String>() {
             @Override
             public JSONObject convert(String from) {
@@ -33,8 +35,8 @@ public class JSONObjectAndStringCollectionCoder {
             }
         });
     }
-
-    public static final <E,T> Collection<E> convertCollection(@NotNull Collection<T> from,@NotNull Converter<E,T> converter){
+    @SuppressWarnings("unchecked")
+    public static <E,T> Collection<E> convertCollection(@NotNull Collection<T> from,@NotNull Converter<E,T> converter){
         if (from.isEmpty()) {
             return null;
         }
