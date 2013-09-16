a379f0d2076fcb10a73a8649eabd40a52742cf4b
cikelengfeng <502805412@qq.com>
2013-09-16 22:59:16 +0800

    整理了NotificationManager的代码

--- src/com/yuedu/fm/YueduNotificationManager.java
+++ src/com/yuedu/fm/YueduNotificationManager.java
@@ -20,34 +20,41 @@ public enum YueduNotificationManager {
     private static final int ONGOING_NOTIFICATION_ID = 0x77<<7;
 
     public void setPlayButtonPlaying(Context context ,boolean b) {
-        Intent intent = new Intent(context,MainPlayer.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        PendingIntent pendingIntent = getPendingIntent(context);
 
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.notification_layout);
         int res = b ? R.drawable.pause : R.drawable.play;
         remoteViews.setInt(R.id.notification_pause_or_play,"setImageResource",res);
 
-        Intent nextIntent = new Intent(YueduService.PLAYER_RECEIVING_BROADCAST_ACTION);
-        nextIntent.addCategory(YueduService.PLAYER_RECEIVING_BROADCAST_CATEGORY_PLAY_NEXT);
-        PendingIntent nextPendingIntent = PendingIntent.getBroadcast(context, 0, nextIntent, 0);
-        Intent switchIntent =  new Intent(YueduService.PLAYER_RECEIVING_BROADCAST_ACTION);
-        switchIntent.addCategory(YueduService.PLAYER_RECEIVING_BROADCAST_CATEGORY_SWITCH_PLAYSTATE);
-        PendingIntent switchPendingIntent = PendingIntent.getBroadcast(context, 0, switchIntent, 0);
-        remoteViews.setOnClickPendingIntent(R.id.notification_next,nextPendingIntent);
-        remoteViews.setOnClickPendingIntent(R.id.notification_pause_or_play,switchPendingIntent);
+        setRemoteActions(context, remoteViews);
 
-        Notification notification = new NotificationCompat.Builder(context).setContentIntent(pendingIntent).setSmallIcon(R.drawable.ic_launcher).setContent(remoteViews).build();
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(ONGOING_NOTIFICATION_ID,notification);
+        notify(context, getNotification(context, pendingIntent, remoteViews));
     }
 
     public void updateTitle(Context context) {
-        Intent intent = new Intent(context,MainPlayer.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        PendingIntent pendingIntent = getPendingIntent(context);
 
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.notification_layout);
         remoteViews.setCharSequence(R.id.notification_tune_name, "setText",DataAccessor.SINGLE_INSTANCE.getPlayingTune().title);
 
+        setRemoteActions(context, remoteViews);
+
+        notify(context,getNotification(context,pendingIntent,remoteViews));
+    }
+
+    public void showForegroundNotification(Service service) {
+        PendingIntent pendingIntent = getPendingIntent(service);
+
+        RemoteViews remoteViews = new RemoteViews(service.getPackageName(),R.layout.notification_layout);
+        remoteViews.setCharSequence(R.id.notification_tune_name, "setText",DataAccessor.SINGLE_INSTANCE.getPlayingTune().title);
+
+        setRemoteActions(service, remoteViews);
+
+        service.startForeground(ONGOING_NOTIFICATION_ID, getNotification(service,pendingIntent,remoteViews));
+
+    }
+
+    private void setRemoteActions(Context context, RemoteViews remoteViews) {
         Intent nextIntent = new Intent(YueduService.PLAYER_RECEIVING_BROADCAST_ACTION);
         nextIntent.addCategory(YueduService.PLAYER_RECEIVING_BROADCAST_CATEGORY_PLAY_NEXT);
         PendingIntent nextPendingIntent = PendingIntent.getBroadcast(context, 0, nextIntent, 0);
@@ -56,31 +63,21 @@ public enum YueduNotificationManager {
         PendingIntent switchPendingIntent = PendingIntent.getBroadcast(context, 0, switchIntent, 0);
         remoteViews.setOnClickPendingIntent(R.id.notification_next,nextPendingIntent);
         remoteViews.setOnClickPendingIntent(R.id.notification_pause_or_play,switchPendingIntent);
+    }
 
-        Notification notification = new NotificationCompat.Builder(context).setContentIntent(pendingIntent).setSmallIcon(R.drawable.ic_launcher).setContent(remoteViews).build();
+    private void notify(Context context,Notification notification) {
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(ONGOING_NOTIFICATION_ID,notification);
     }
 
-    public void showForegroundNotification(Service service) {
-        Intent intent = new Intent(service.getApplicationContext(),MainPlayer.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(service.getApplicationContext(), 0, intent, 0);
+    private PendingIntent getPendingIntent(Context context) {
+        Intent intent = new Intent(context,MainPlayer.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        return pendingIntent;
+    }
 
-        RemoteViews remoteViews = new RemoteViews(service.getPackageName(),R.layout.notification_layout);
-        remoteViews.setCharSequence(R.id.notification_tune_name, "setText",DataAccessor.SINGLE_INSTANCE.getPlayingTune().title);
-
-        Intent nextIntent = new Intent(YueduService.PLAYER_RECEIVING_BROADCAST_ACTION);
-        nextIntent.addCategory(YueduService.PLAYER_RECEIVING_BROADCAST_CATEGORY_PLAY_NEXT);
-        PendingIntent nextPendingIntent = PendingIntent.getBroadcast(service.getApplicationContext(), 0, nextIntent, 0);
-        Intent switchIntent =  new Intent(YueduService.PLAYER_RECEIVING_BROADCAST_ACTION);
-        switchIntent.addCategory(YueduService.PLAYER_RECEIVING_BROADCAST_CATEGORY_SWITCH_PLAYSTATE);
-        PendingIntent switchPendingIntent = PendingIntent.getBroadcast(service.getApplicationContext(), 0, switchIntent, 0);
-        remoteViews.setOnClickPendingIntent(R.id.notification_next,nextPendingIntent);
-        remoteViews.setOnClickPendingIntent(R.id.notification_pause_or_play,switchPendingIntent);
-
-        Notification notification = new NotificationCompat.Builder(service).setContentIntent(pendingIntent).setSmallIcon(R.drawable.ic_launcher).setContent(remoteViews).build();
-        service.startForeground(ONGOING_NOTIFICATION_ID, notification);
-
+    private Notification getNotification(Context context,PendingIntent contentIntent,RemoteViews content) {
+        return  new NotificationCompat.Builder(context).setContentIntent(contentIntent).setSmallIcon(R.drawable.ic_launcher).setContent(content).build();
     }
 
     public void stopForeground(Service service) {
